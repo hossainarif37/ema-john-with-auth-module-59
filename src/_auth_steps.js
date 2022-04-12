d07e3a374d7734b49725452d74bcc30452647fbf
hossainarif37 <hossainarif4937@gmail.com>
2022-04-12 18:13:25 +0600

    init file updated

---
+++ src/_auth_steps.js
@@ -0,0 +1,16 @@
+/*
+* create a new project in console.google.com
+* npm install firebase
+* create firebase.init.js and import getAuth to export auth
+* firebase settings > authentication > enable email and password auth
+
+
+*/
+
+// ? hosting steps
+/* 
+* npm install -g firebase-tools
+* firebase login
+* firebase init
+* firebase deploy
+*/
